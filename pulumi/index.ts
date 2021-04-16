8d59e76af2b47f42844f8dd5226ccafc55f59c36
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-04-15 21:09:18 -0700

    remove chart dep

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -32,19 +32,49 @@ const pulumiTaggedImages = Object.entries(tezosK8sImages).reduce(
 
 helmValues["tezos_k8s_images"] = pulumiTaggedImages
 
-const vpc = new awsx.ec2.Vpc(chainName + "-vpc", {});
+const tags = { "project": "subnets", "owner": "pulumi"};
 
-// For now, we set the number of EKS nodes to be the number of
-// Tezos bakers/nodes divided by 12.  We should fix this to
-// be a little more automagic.  Why 12?  It's a guess currently,
-// we should experiment a little to get a better number.  We add
-// three because that's the minimum.
+// Create a VPC with subnets that are tagged for load balancer usage.
+const vpc = new awsx.ec2.Vpc("vpc",
+    {
+        tags: {"Name": `${projectName}`, ...tags},
+        subnets: [
+            // Tag subnets for specific load-balancer usage.
+            // Any non-null tag value is valid.
+            // See:
+            //  - https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html
+            //  - https://github.com/pulumi/pulumi-eks/issues/196
+            //  - https://github.com/pulumi/pulumi-eks/issues/415
+            {type: "public", tags: {"kubernetes.io/role/elb": "1", ...tags}},
+            {type: "private", tags: {"kubernetes.io/role/internal-elb": "1", ...tags}},
+        ],
+    },
+    {
+        // Inform pulumi to ignore tag changes to the VPCs or subnets, so that
+        // tags auto-added by AWS EKS do not get removed during future
+        // refreshes and updates, as they are added outside of pulumi's management
+        // and would be removed otherwise.
+        // See: https://github.com/pulumi/pulumi-eks/issues/271#issuecomment-548452554
+        transformations: [(args: any) => {
+            if (args.type === "aws:ec2/vpc:Vpc" || args.type === "aws:ec2/subnet:Subnet") {
+                return {
+                    props: args.props,
+                    opts: pulumi.mergeOptions(args.opts, { ignoreChanges: ["tags"] }),
+                };
+            }
+            return undefined;
+        }],
+    },
+);
 
-const numBakers = helmValues.nodes?.baking?.length || 0
-const numRegularNodes  = helmValues.nodes?.regular?.length || 0
-
-const totalTezosNodes = numBakers + numRegularNodes
-const desiredClusterCapacity = Math.round(totalTezosNodes / 12 + 3);
+// Create a cluster using the VPC and subnets.
+const cluster = new eks.Cluster(`${projectName}`, {
+    vpcId: vpc.id,
+    publicSubnetIds: vpc.publicSubnetIds,
+    privateSubnetIds: vpc.privateSubnetIds,
+    tags,
+});
+V
 
 // Create an EKS cluster.
 const cluster = new eks.Cluster(chainName + "-chain", {
